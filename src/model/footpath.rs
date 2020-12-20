3916d2a3cc461e70b4cc7b60656bfe9ffc1c5761
Elias Wendt <git@eliaswendt.de>
2020-12-20 01:25:29 +0100

    Implement footpath as object

--- src/model/footpath.rs
+++ src/model/footpath.rs
@@ -1,17 +1,24 @@
 use std::collections::HashMap;
 
 pub struct Footpath {
-    from_station: String,
-    to_station: String,
-    duration: u64
+    pub from_station: String,
+    pub to_station: String,
+    pub duration: u64
 }
 
 impl Footpath {
-    pub fn from_map(map: HashMap<String, String>) -> Self {
-        Self {
-            from_station: map.get("from_station").unwrap().clone(),
-            to_station: map.get("to_station").unwrap().clone(),
-            duration: map.get("duration").unwrap().parse().unwrap()
+    pub fn from_maps_to_vec(footpath_maps: &Vec<HashMap<String, String>>) -> Vec<Self> {
+
+        let mut footpaths_vec = Vec::with_capacity(footpath_maps.len());
+
+        for footpath_map in footpath_maps.iter() {
+            footpaths_vec.push(Self {
+                from_station: footpath_map.get("from_station").unwrap().clone(),
+                to_station: footpath_map.get("to_station").unwrap().clone(),
+                duration: footpath_map.get("duration").unwrap().parse().unwrap()
+            });
         }
+
+        footpaths_vec
     }
 }
