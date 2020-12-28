d7304a6704fe416658766b2417c582c50fadaab0
Elias Wendt <ewendt@macbook1.fritz.box>
2020-12-28 10:15:36 +0100

    minor test runs

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,17 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("run with {} <folder_path>", args[0]);
+        println!("run with {} <csv_folder_path>", args[0]);
         return;
     }
 
     let model = model::Model::with_stations_footpaths_and_trips(&args[1]);
 
-    model.find_solutions("sample_data/groups.csv", 60);
+    model.find_solutions(&format!("{}/groups.csv", &args[1]), 60);
 
-    let dot_code = model.to_dot();
+    // let dot_code = model.to_dot();
 
-    BufWriter::new(File::create("graphs/graph.dot").unwrap()).write(
-        dot_code.as_bytes()
-    ).unwrap();
+    // BufWriter::new(File::create("graphs/graph.dot").unwrap()).write(
+    //     dot_code.as_bytes()
+    // ).unwrap();
 }
