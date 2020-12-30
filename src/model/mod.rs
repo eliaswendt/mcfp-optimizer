d800bef903544bdae4032a6c4cd8c72d66bb62a0
Elias Wendt <git@eliaswendt.de>
2020-12-30 16:34:03 +0100

    sync

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -5,10 +5,10 @@ pub mod trip;
 pub mod algo;
 
 use group::Group;
-use petgraph::{EdgeDirection::Outgoing, Graph, graph::{NodeIndex, EdgeIndex, GraphIndex, DiGraph}, visit::{IntoEdgeReferences, IntoEdges}};
+use petgraph::{EdgeDirection::Outgoing, Graph, graph::{NodeIndex, EdgeIndex, GraphIndex, DiGraph}, visit::{IntoEdgeReferences, IntoEdges, IntoNeighborsDirected, NodeCount}};
 use petgraph::algo::{dijkstra, min_spanning_tree, all_simple_paths};
 use petgraph::dot::{Dot, Config};
-use std::{collections::{HashMap, HashSet}, iter::{FromFn, FromIterator, from_fn}};
+use std::{collections::{HashMap, HashSet}, hash::Hash, iter::{FromFn, FromIterator, from_fn}};
 
 use std::fs::File;
 use std::io::{prelude::*, BufWriter};
@@ -568,8 +568,15 @@ impl Model {
         end_node_condition: fn(NodeIndex) -> bool, 
         edge_cost: fn(EdgeIndex) -> u64, 
         max_costs: u64
-    ) {
+    ) -> impl Iterator<Item = TargetCollection>
+    where
+    G: NodeCount,
+    G: IntoNeighborsDirected,
+    G::NodeId: Eq + Hash,
+    TargetCollection: FromIterator<G::NodeId>,
+    {
 
+        // loop detection: save all already visited nodes
         let mut visited: IndexSet<NodeIndex> = IndexSet::from_iter(Some(start_node));
         let mut stack = vec![self.graph.neighbors_directed(start_node, Outgoing)];
 
@@ -577,7 +584,73 @@ impl Model {
 
     }
 
-
+    pub fn all_simple_paths2<TargetCollection, G>(
+        graph: G,
+        from: G::NodeId,
+        to: G::NodeId,
+        min_intermediate_nodes: usize,
+        max_intermediate_nodes: Option<usize>,
+    ) -> impl Iterator<Item = TargetCollection>
+    where
+        G: NodeCount,
+        G: IntoNeighborsDirected,
+        G::NodeId: Eq + Hash,
+        TargetCollection: FromIterator<G::NodeId>,
+    {
+        // how many nodes are allowed in simple path up to target node
+        // it is min/max allowed path length minus one, because it is more appropriate when implementing lookahead
+        // than constantly add 1 to length of current path
+        let max_length = if let Some(l) = max_intermediate_nodes {
+            l + 1
+        } else {
+            graph.node_count() - 1
+        };
+    
+        let min_length = min_intermediate_nodes + 1;
+    
+        // list of visited nodes
+        let mut visited: IndexSet<G::NodeId> = IndexSet::from_iter(Some(from));
+        // list of childs of currently exploring path nodes,
+        // last elem is list of childs of last visited node
+        let mut stack = vec![graph.neighbors_directed(from, Outgoing)];
+    
+        from_fn(move || {
+            while let Some(children) = stack.last_mut() {
+                if let Some(child) = children.next() {
+                    if visited.len() < max_length {
+                        if child == to {
+                            if visited.len() >= min_length {
+                                let path = visited
+                                    .iter()
+                                    .cloned()
+                                    .chain(Some(to))
+                                    .collect::<TargetCollection>();
+                                return Some(path);
+                            }
+                        } else if !visited.contains(&child) {
+                            visited.insert(child);
+                            stack.push(graph.neighbors_directed(child, Outgoing));
+                        }
+                    } else {
+                        if (child == to || children.any(|v| v == to)) && visited.len() >= min_length {
+                            let path = visited
+                                .iter()
+                                .cloned()
+                                .chain(Some(to))
+                                .collect::<TargetCollection>();
+                            return Some(path);
+                        }
+                        stack.pop();
+                        visited.pop();
+                    }
+                } else {
+                    stack.pop();
+                    visited.pop();
+                }
+            }
+            None
+        })
+    }
 
     // pub fn shortest_paths<TargetColl, G>(
     //     &self, 
