df8a3f12d5d858d1578cdd55ff5d88556b36fb16
Elias Wendt <git@eliaswendt.de>
2021-01-02 18:37:32 +0100

    Add individual edge costs and IDDFS

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -105,6 +105,28 @@ pub enum EdgeWeight {
 
 impl EdgeWeight {
 
+    // maps every edge to some virtual cost for improved DFS (aka. effort/expense to "take" the edge)
+    pub fn cost(&self) -> u64 {
+        match self {
+            Self::RideToStation {duration: _, capacity: _, utilization: _} => 1,
+            Self::StayInTrain {duration: _} => 5,
+            Self::Alight {duration: _} => 5,
+            Self::StayAtStation {duration: _} => 5,
+            Self::WalkToStation {duration: _} => 10,
+            Self::Embark => 5,
+            Self::MainArrivalRelation => 0 // no cost, just a "meta" path
+        }
+    }
+
+    pub fn is_stay_in_train(&self) -> bool {
+        match self {
+            Self::StayInTrain {
+                duration: _, 
+            } => true,
+            _ => false,
+        }
+    }
+
     /// is RideToStation Edge
     pub fn is_ride_to_station(&self) -> bool {
         match self {
@@ -384,6 +406,8 @@ impl Model {
 
         println!("successful_footpaths: {}, failed_footpaths: {}", successful_footpath_counter, failed_footpath_counter);
 
+        println!("node_count={}, edge_count={}", graph.node_count(), graph.edge_count());
+
         Self {
             graph,
             stations_departures,
@@ -423,9 +447,10 @@ impl Model {
                 &self.graph, 
                 from_node_index, 
                 to_node_index, //|node| node.is_arrival_at_station(&group_value.destination), // dynamic condition for dfs algorithm to find arrival node
+
                 group_value.passengers as u64, 
                 max_duration, 
-                8 // todo: evaluate best value here
+                100
             );
 
             paths_recursive.sort_unstable_by_key(|(remaining_duration, _)| *remaining_duration);
@@ -532,15 +557,62 @@ impl Model {
         )
     }
 
+
+    // fn all_paths_iddfs(
+    //     graph: &DiGraph<NodeWeight, EdgeWeight>,
+    //     from: NodeIndex,
+    //     to: NodeIndex, // condition that determines whether goal node was found
+        
+    //     min_capacity: u64,
+    //     max_duration: u64,
+    //     max_alights: u64,
+    //     max_footpaths: u64,
+
+    //     depth_steps: u64, 
+    //     min_depth: u64,
+    //     max_depth: u64 // maximum number of transfers to follow
+    // ) -> Vec<(u64, Vec<EdgeIndex>)> {
+
+    //     let mut result = Vec::new();
+
+    //     // increase depth in 4 steps
+    //     let depth_step = (max_depth - min_depth) / depth_steps;
+
+    //     for i in 0..depth_steps+1 {
+
+    //         let current_depth = min_depth + i * depth_step;
+
+    //         println!("[iddfs()] trying with depth={}", current_depth);
+
+    //         result = Self::all_paths_dfs_recursive(
+    //             graph, 
+    //             from, 
+    //             to, 
+    //             min_capacity, 
+    //             max_duration, 
+    //             budget, 
+    //             max_footpaths, 
+    //             current_depth
+    //         );
+
+    //         if result.len() > 0 {
+    //             break
+    //         }
+    //     }
+
+    //     result
+    // }
+
     // launcher of recursive implementation of dfs
     // returns a vec of paths along with their remaining_duration
-    pub fn all_paths_dfs_recursive(
+    fn all_paths_dfs_recursive(
         graph: &DiGraph<NodeWeight, EdgeWeight>,
         from: NodeIndex,
         to: NodeIndex, // condition that determines whether goal node was found
+        
         min_capacity: u64,
         max_duration: u64,
-        max_alights: u64 // maximum number of transfers to follow
+        max_budget: u64, // initial search budget (each edge has cost that needs to be payed)
     ) -> Vec<(u64, Vec<EdgeIndex>)> {
 
         // println!("all_paths_dfs(from={:?}, to={:?}, min_capacity={}, max_duration={})", from, to, min_capacity, max_duration);
@@ -554,13 +626,15 @@ impl Model {
             from, 
             to, 
             &mut visited, 
+
             min_capacity, 
             max_duration, 
-            max_alights + 1
+            max_budget,
         );
 
         paths
     }
+
 
     fn all_paths_dfs_recursive_helper(
         graph: &DiGraph<NodeWeight, EdgeWeight>,
@@ -568,9 +642,11 @@ impl Model {
         current: NodeIndex, 
         to: NodeIndex, 
         visited: &mut Vec<EdgeIndex>, // vec of visited edges (in order of visit)
+
+        // recursion anchors (if zero)
         min_capacity: u64,
-        remaining_duration: u64, // if zero -> recursion anchor
-        remaining_alights: u64 // if zero -> recursion anchor
+        remaining_duration: u64,
+        remaining_budget: u64,
     ) {
 
         // println!("all_paths_dfs_recursive(current={:?}, goal={:?}, visited.len()={}, min_capacity={}, remaining_duration={})", current, to, visited.len(), min_capacity, remaining_duration);
@@ -583,24 +659,18 @@ impl Model {
                 (remaining_duration, visited.iter().cloned().collect())
             );
 
-        } else if remaining_alights > 0 {
+        } else {
             let mut walker = graph.neighbors_directed(current, Outgoing).detach();
 
             // iterate over all outgoing edges
             while let Some((next_edge, next_node)) = walker.next(graph) {
 
                 let edge_weight = &graph[next_edge];
-                let edge_duration = edge_weight.get_duration();
+                let edge_weight_duration = edge_weight.get_duration();
+                let edge_weight_cost = edge_weight.cost();
 
-                if edge_duration <= remaining_duration && edge_weight.get_remaining_capacity() >= min_capacity {
-                    // edge can handle the minium required capacity and does not take longer then the remaining duration        
-
-                    let new_remaining_alights = if edge_weight.is_alight() {
-                        remaining_alights - 1
-                    } else {
-                        remaining_alights
-                    };
-
+                if edge_weight_duration <= remaining_duration && edge_weight.get_remaining_capacity() >= min_capacity && edge_weight_cost <= remaining_budget {
+                    // edge can handle the minium required capacity and does not take longer then the remaining duration
 
                     // add next_edge for next call
                     visited.push(next_edge);
@@ -612,8 +682,8 @@ impl Model {
                         to, 
                         visited, 
                         min_capacity, 
-                        remaining_duration - edge_duration, 
-                        new_remaining_alights
+                        remaining_duration - edge_weight_duration,
+                        remaining_budget - edge_weight_cost,
                     );
 
                     // remove next_edge from visited
